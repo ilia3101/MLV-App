32399ad510208ea05dda7b468a7d6fdd37ca0b29
Ilia Sibiryakov <stevefredpatrick@gmail.com>
2017-07-20 20:16:03 +0100

    Fix binning_test (was broken from recent changes)
    It was broken, now it works again. If you look at the code, keep in mind: speed is not relevant here

--- platform/binning_test/main.c
+++ platform/binning_test/main.c
@@ -1,5 +1,3 @@
-/* This should be converted into a commanline version */
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
@@ -15,10 +13,20 @@
 mlvObject_t * video;
 processingObject_t * processingsettings;
 
+float sum(float * array, int count)
+{
+    float sum = 0.0f;
+    for (int i = 0; i < count; ++i)
+    {
+        sum += array[i];
+    }
+    return sum;
+}
+
 int main()
 {
     /* Can be any clip (cropmode/fullressilent is best) */
-    video = initMlvObjectWithClip("../../mlvsamples/pict_index2.MLV");
+    video = initMlvObjectWithClip("../../../MLV_CLIP.mlv");
     processingsettings = initProcessingObject();
 
     /* Link video with processing settings */
@@ -31,6 +39,7 @@ int main()
 
     float * frame = malloc( sizeof(float) * pixel_size );
 
+    /* Just use frame 2 */
     getMlvRawFrameFloat(video, 2, frame);
 
 
@@ -42,6 +51,15 @@ int main()
     /* 1/3 size */
     float * small_frame_5d2 = malloc( sizeof(float) * third_frame_size );
     float * small_frame_5d3 = malloc( sizeof(float) * third_frame_size );
+
+    /* binning weights */
+    float bw_5d3[] = { 1.0f, 1.0f, 1.0f,
+                       1.0f, 1.0f, 1.0f,
+                       1.0f, 1.0f, 1.0f };
+    
+    float bw_5d2[] = { 0.0f, 0.0f, 0.0f,
+                       1.0f, 1.0f, 1.0f,
+                       0.0f, 0.0f, 0.0f, };
 
     for (int y = 0; y < third_height; ++y)
     {
@@ -60,20 +78,37 @@ int main()
             float * output_5d3 = small_frame_5d3 + (loc_y + x);
 
             /* Bad binning */
-            * output_5d2 = (input_pix[0] + input_pix[-2] + input_pix[2]) / 3;
+            * output_5d2 = (
+                (input_pix_above[0] * bw_5d2[0] + input_pix_above[-2] * bw_5d2[1] + input_pix_above[2] * bw_5d2[2])
+                 + (input_pix[0] * bw_5d2[3]    +    input_pix[-2] * bw_5d2[4]    +    input_pix[2] * bw_5d2[5]) +
+                (input_pix_below[0] * bw_5d2[6] + input_pix_below[-2] * bw_5d2[7] + input_pix_below[2] * bw_5d2[8])
+            ) / sum(bw_5d2, 9);
             /* Good binning */
             * output_5d3 = (
-                (input_pix_above[0] + input_pix_above[-2] + input_pix_above[2])
-                 + (input_pix[0]    +    input_pix[-2]    +    input_pix[2]) +
-                (input_pix_below[0] + input_pix_below[-2] + input_pix_below[2])
-            ) / 9;
+                (input_pix_above[0] * bw_5d3[0] + input_pix_above[-2] * bw_5d3[1] + input_pix_above[2] * bw_5d3[2])
+                 + (input_pix[0] * bw_5d3[3]    +    input_pix[-2] * bw_5d3[4]    +    input_pix[2] * bw_5d3[5]) +
+                (input_pix_below[0] * bw_5d3[6] + input_pix_below[-2] * bw_5d3[7] + input_pix_below[2] * bw_5d3[8])
+            ) / sum(bw_5d3, 9);
+
+            /* Just to make the image easier to look at.... speed doesnt matter ok */
+            * output_5d2 -= getMlvBlackLevel(video) * 4;
+            * output_5d3 -= getMlvBlackLevel(video) * 4;
+
+            * output_5d3 /= 65535.0f;
+            * output_5d2 /= 65535.0f;
+
+            * output_5d3 = powf(* output_5d3, 0.45);
+            * output_5d2 = powf(* output_5d2, 0.45);
+            
+            * output_5d3 *= 65535.0f;
+            * output_5d2 *= 65535.0f;
         }
     }
 
     uint8_t * bitmap_small = malloc( third_frame_size * 3 * sizeof(uint8_t) );
 
-    /* Pay zero attention to the 'imagestruct' and bitmap BS, it was pretty much my first C project */
-    imagestruct small_image = { third_width, third_height, bitmap_small, 0, 0 };
+    /* Pay zero attention to the 'imagestruct' and bitmap bits, it was pretty much my first C project */
+    imagestruct small_image = { third_width, third_height, bitmap_small, 1 };
 
     /* Copy and save 5D2 */
     for (int i = 0; i < third_frame_size; ++i)
@@ -83,7 +118,7 @@ int main()
         bitmap_small[i*3+2] = bitmap_small[i * 3]; 
     }
 
-    write_bmp3_24(small_image, "5D2.bmp");
+    write_bmp3_24(&small_image, "5D2.bmp");
 
     /* Copy and save 5D3 */
     for (int i = 0; i < third_frame_size; ++i)
@@ -93,12 +128,8 @@ int main()
         bitmap_small[i*3+2] = bitmap_small[i * 3]; 
     }
 
-    write_bmp3_24(small_image, "5D3.bmp");
+    write_bmp3_24(&small_image, "5D3.bmp");
 
-
-
-
-    /* This is my old VERY CRAP bmp writer, sorry */
 
     /* Full res output */
     uint8_t * bitmap = malloc( pixel_size * 3 * sizeof(uint8_t) );
@@ -111,8 +142,8 @@ int main()
     }
 
     /* Full res */
-    imagestruct TestImage = { original_width, original_height, bitmap, 0, 0 };
-    write_bmp3_24(TestImage, "output.bmp");
+    imagestruct TestImage = { original_width, original_height, bitmap, 1 };
+    write_bmp3_24(&TestImage, "output.bmp");
 
     freeMlvObject(video);
     freeProcessingObject(processingsettings);
