e6a17168e62502efd0eedf87290fac68b84c1126
masc4ii <markus.schweitzer@gmx.de>
2017-09-20 22:01:53 +0200

    QtApp: temporary GUI integration of llrawproc. Temporary because: just changing values via mlvObject, that is really not nice. Better would be changing the values via methods (or functions). Known bug: the position slider must be changed to refresh the picture, because that is only possibility to refresh a already debayered picture.

--- platform/qt/MainWindow.cpp
+++ platform/qt/MainWindow.cpp
@@ -2036,3 +2036,56 @@ void MainWindow::on_actionShowZebras_triggered()
 {
     m_frameChanged = true;
 }
+
+//Combobox Focus Pixel changed
+void MainWindow::on_comboBoxFocusPixelSwitch_currentIndexChanged(int index)
+{
+    m_pMlvObject->llrawproc->focus_pixels = index;
+    m_frameChanged = true;
+}
+
+//Combobox Bad Pixel changed
+void MainWindow::on_comboBoxBadPixelsSwitch_currentIndexChanged(int index)
+{
+    m_pMlvObject->llrawproc->bad_pixels = index;
+    m_frameChanged = true;
+}
+
+//Combobox Chroma Smooth changed
+void MainWindow::on_comboBoxChromaSmoothSwitch_currentIndexChanged(int index)
+{
+    switch( index )
+    {
+    case 0: m_pMlvObject->llrawproc->chroma_smooth = 0;
+        break;
+    case 1: m_pMlvObject->llrawproc->chroma_smooth = 2;
+        break;
+    case 2: m_pMlvObject->llrawproc->chroma_smooth = 3;
+        break;
+    case 3: m_pMlvObject->llrawproc->chroma_smooth = 5;
+        break;
+    default: m_pMlvObject->llrawproc->chroma_smooth = 0;
+    }
+    m_frameChanged = true;
+}
+
+//Combobox Pattern Noise changed
+void MainWindow::on_comboBoxPatternNoiseSwitch_currentIndexChanged(int index)
+{
+    m_pMlvObject->llrawproc->pattern_noise = index;
+    m_frameChanged = true;
+}
+
+//Combobox Vertical Stripes changed
+void MainWindow::on_comboBoxVerticalStripesSwitch_currentIndexChanged(int index)
+{
+    m_pMlvObject->llrawproc->vertical_stripes = index;
+    m_frameChanged = true;
+}
+
+//Value Deflicker Target changed
+void MainWindow::on_spinBoxDeflickerTarget_valueChanged(int arg1)
+{
+    m_pMlvObject->llrawproc->deflicker_target = arg1;
+    m_frameChanged = true;
+}
--- platform/qt/MainWindow.h
+++ platform/qt/MainWindow.h
@@ -104,8 +104,13 @@ private slots:
     void on_actionFullscreen_triggered(bool checked);
     void exportHandler( void );
     void on_actionPlay_triggered(bool checked);
-
     void on_actionShowZebras_triggered();
+    void on_comboBoxFocusPixelSwitch_currentIndexChanged(int index);
+    void on_comboBoxBadPixelsSwitch_currentIndexChanged(int index);
+    void on_comboBoxChromaSmoothSwitch_currentIndexChanged(int index);
+    void on_comboBoxPatternNoiseSwitch_currentIndexChanged(int index);
+    void on_comboBoxVerticalStripesSwitch_currentIndexChanged(int index);
+    void on_spinBoxDeflickerTarget_valueChanged(int arg1);
 
 private:
     Ui::MainWindow *ui;
