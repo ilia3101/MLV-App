8de271402efb27a7540da79dbec25690f0782c3a
Ilia Sibiryakov <iliasibiryakov@googlemail.com>
2017-11-18 17:18:27 +0000

    Cocoa App: more debug info in title

--- platform/cocoa/compile_time_code.m
+++ platform/cocoa/compile_time_code.m
@@ -14,13 +14,28 @@
 
 int main(int argc, char * argv[])
 {
-    /* Generate App title with build info in a temporary .h file */
-    char * app_name = malloc(1024);
-    char * host_name = malloc(1024);
+    /* Generate App title with ultimate debugging build info in a temporary .h file */
+    char app_name[1024];
+    char host_name[1024];
+    char commit[1024] = {0};
 
-    gethostname(host_name, 1023);
+    gethostname(host_name, 1024);
 
-    snprintf(app_name, 1023, APP_NAME " (" __DATE__ " " __TIME__ " @%s)", host_name);
+    /* Get latest commit id to put in app name */
+    system("git rev-parse HEAD >> commithash");
+    FILE * commithash = fopen("commithash", "rb");
+    fseek(commithash, 0, SEEK_END);
+    int size = ftell(commithash);
+    fseek(commithash, 0, SEEK_SET);
+    fread(commit, size, sizeof(char), commithash);
+    fclose(commithash);
+    system("rm commithash");
+    commit[strlen(commit)-1] = 0;
+
+    /* Get macOS version */
+    const char * _Nullable macOS_version = [[[NSDictionary dictionaryWithContentsOfFile:@"/System/Library/CoreServices/SystemVersion.plist"] objectForKey:@"ProductVersion"] UTF8String];
+
+    snprintf(app_name, 1024, APP_NAME " (" __DATE__ " " __TIME__ " @%s [%s] %s)", host_name, macOS_version, commit);
 
     printf("Initial app name: %s\n", app_name);
 
@@ -40,7 +55,4 @@ int main(int argc, char * argv[])
     fprintf(app_name_header, "#endif\n");
 
     fclose(app_name_header);
-
-    free(host_name);
-    free(app_name);
 }
