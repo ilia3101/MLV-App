3089876106268c2cd975bb48056d9b5e57ebd07d
bouncyball-git <infograb@gol.ge>
2017-12-18 23:09:53 +0400

    info dialog: shutter angle info updated

--- platform/qt/MainWindow.cpp
+++ platform/qt/MainWindow.cpp
@@ -528,6 +528,10 @@ int MainWindow::openMlv( QString fileName )
     //Init Render Thread
     m_pRenderThread->init( m_pMlvObject, m_pRawImage );
 
+    //Calculate shutter flavors :)
+    float shutterSpeed = 1000000.0f / (float)(getMlvShutter( m_pMlvObject ));
+    float shutterAnge = getMlvFramerate( m_pMlvObject ) * 360.0f / shutterSpeed;
+
     //Set Clip Info to Dialog
     m_pInfoDialog->ui->tableWidget->item( 0, 1 )->setText( QString( "%1" ).arg( (char*)getMlvCamera( m_pMlvObject ) ) );
     m_pInfoDialog->ui->tableWidget->item( 1, 1 )->setText( QString( "%1" ).arg( (char*)getMlvLens( m_pMlvObject ) ) );
@@ -536,10 +540,10 @@ int MainWindow::openMlv( QString fileName )
     m_pInfoDialog->ui->tableWidget->item( 4, 1 )->setText( QString( "%1" ).arg( (int)getMlvFrames( m_pMlvObject ) ) );
     m_pInfoDialog->ui->tableWidget->item( 5, 1 )->setText( QString( "%1 fps" ).arg( getMlvFramerate( m_pMlvObject ) ) );
     m_pInfoDialog->ui->tableWidget->item( 6, 1 )->setText( QString( "%1 mm" ).arg( getMlvFocalLength( m_pMlvObject ) ) );
-    m_pInfoDialog->ui->tableWidget->item( 7, 1 )->setText( QString( "%1 µs" ).arg( getMlvShutter( m_pMlvObject ) ) );
+    m_pInfoDialog->ui->tableWidget->item( 7, 1 )->setText( QString( "1/%1,  %2 degree  (%3 µs)" ).arg( (uint16_t)(shutterSpeed + 0.5f) ).arg( (uint16_t)(shutterAnge + 0.5f) ).arg( getMlvShutter( m_pMlvObject )) );
     m_pInfoDialog->ui->tableWidget->item( 8, 1 )->setText( QString( "f/%1" ).arg( getMlvAperture( m_pMlvObject ) / 100.0, 0, 'f', 1 ) );
     m_pInfoDialog->ui->tableWidget->item( 9, 1 )->setText( QString( "%1" ).arg( (int)getMlvIso( m_pMlvObject ) ) );
-    m_pInfoDialog->ui->tableWidget->item( 10, 1 )->setText( QString( "%1 bits, %2" ).arg( getMlvBitdepth( m_pMlvObject ) ).arg( getMlvCompression( m_pMlvObject ) ) );
+    m_pInfoDialog->ui->tableWidget->item( 10, 1 )->setText( QString( "%1 bits,  %2" ).arg( getMlvBitdepth( m_pMlvObject ) ).arg( getMlvCompression( m_pMlvObject ) ) );
     m_pInfoDialog->ui->tableWidget->item( 11, 1 )->setText( QString( "%1-%2-%3 / %4:%5:%6" )
                                                             .arg( getMlvTmYear(m_pMlvObject) )
                                                             .arg( getMlvTmMonth(m_pMlvObject), 2, 10, QChar('0') )
