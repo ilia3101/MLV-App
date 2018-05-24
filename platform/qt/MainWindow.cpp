00d966b21cb4c6ebcb8323f88d1ae2232d6407b5
masc4ii <markus.schweitzer@gmx.de>
2018-05-24 19:17:13 +0200

    QtApp v0.16 alpha

--- platform/qt/MainWindow.cpp
+++ platform/qt/MainWindow.cpp
@@ -34,8 +34,8 @@
 #include "Updater/updaterUI/cupdaterdialog.h"
 
 #define APPNAME "MLV App"
-#define VERSION "0.15 alpha"
-#define GITVERSION "QTv0.15alpha"
+#define VERSION "0.16 alpha"
+#define GITVERSION "QTv0.16alpha"
 
 #define FACTOR_DS       22.5
 #define FACTOR_LS       11.2
