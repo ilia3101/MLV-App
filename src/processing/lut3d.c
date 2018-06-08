094b478323853deed55c6206f3f4364bf125f1d3
masc4ii <markus.schweitzer@gmx.de>
2018-06-08 09:33:50 +0200

    Some safety & comments for LUT

--- src/processing/lut3d.c
+++ src/processing/lut3d.c
@@ -39,20 +39,24 @@ void load_lut3d( lut3d_t *lut3d, char *filename )
 //Unload the LUT
 void unload_lut3d( lut3d_t *lut3d )
 {
-    free( lut3d->cube );
+    if( lut3d->cube ) free( lut3d->cube );
     lut3d->dimension = 0;
 }
 
 //Apply LUT on picture
 void apply_lut3d(lut3d_t *lut3d, int width, int height, uint16_t *image)
 {
+    if( !lut3d->dimension || !lut3d->cube ) return;
+
     uint16_t * end = image + (width * height * 3);
 
     for (uint16_t * pix = image; pix < end; pix += 3)
     {
+        //x
         double red = pix[0] * lut3d->dimension / 65536.0;
         double green = pix[1] * lut3d->dimension / 65536.0;
         double blue = pix[2] * lut3d->dimension / 65536.0;
+        //x0
         uint16_t red0 = (uint16_t)red;
         uint16_t green0 = (uint16_t)green;
         uint16_t blue0 = (uint16_t)blue;
@@ -65,6 +69,7 @@ void apply_lut3d(lut3d_t *lut3d, int width, int height, uint16_t *image)
         uint32_t blue0_offs = blue0;
         uint32_t offset0 = ( red0_offs + green0_offs + blue0_offs ) * 3;
 
+        //x1
         uint32_t red1 = red0 + 1;
         uint32_t green1 = green0 + 1;
         uint32_t blue1 = blue0 + 1;
@@ -77,17 +82,21 @@ void apply_lut3d(lut3d_t *lut3d, int width, int height, uint16_t *image)
         uint32_t blue1_offs = blue1;
         uint32_t offset1 = ( red1_offs + green1_offs + blue1_offs ) * 3;
 
+        //y0
         double pix00 = lut3d->cube[ offset0 + 2 ];
         double pix01 = lut3d->cube[ offset1 + 2 ];
         double pix10 = lut3d->cube[ offset0 + 1 ];
+        //y1
         double pix11 = lut3d->cube[ offset1 + 1 ];
         double pix20 = lut3d->cube[ offset0 + 0 ];
         double pix21 = lut3d->cube[ offset1 + 0 ];
 
+        //Interpolation
         double a = pix00 + ( ( pix01 - pix00 ) / ( red1 - red0 ) * ( red - red0 ) );
         double b = pix10 + ( ( pix11 - pix10 ) / ( green1 - green0 ) * ( green - green0 ) );
         double c = pix20 + ( ( pix21 - pix20 ) / ( blue1 - blue0 ) * ( blue - blue0 ) );
 
+        //Output
         pix[0] = LIMIT16( a * 65535.0 );
         pix[1] = LIMIT16( b * 65535.0 );
         pix[2] = LIMIT16( c * 65535.0 );
