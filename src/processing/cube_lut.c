50c84afc2c67a90594116698428c02df1ef55a59
masc4ii <markus.schweitzer@gmx.de>
2018-06-10 13:41:29 +0200

    Corrected LUT domain calculation

--- src/processing/cube_lut.c
+++ src/processing/cube_lut.c
@@ -210,9 +210,9 @@ void apply_lut(lut_t *lut, int width, int height, uint16_t *image)
     for (uint16_t * pix = image; pix < end; pix += 3)
     {
         //x
-        float red   = ( pix[0] * ( lut->dimension - 1 ) / 65536.0 * ( lut->domain_max[0] - lut->domain_min[0] ) ) + lut->domain_min[0];
-        float green = ( pix[1] * ( lut->dimension - 1 ) / 65536.0 * ( lut->domain_max[1] - lut->domain_min[1] ) ) + lut->domain_min[1];
-        float blue  = ( pix[2] * ( lut->dimension - 1 ) / 65536.0 * ( lut->domain_max[2] - lut->domain_min[2] ) ) + lut->domain_min[2];
+        float red   = ( pix[0] * ( lut->dimension - 1 ) / 65536.0 / ( lut->domain_max[0] - lut->domain_min[0] ) ) + lut->domain_min[0];
+        float green = ( pix[1] * ( lut->dimension - 1 ) / 65536.0 / ( lut->domain_max[1] - lut->domain_min[1] ) ) + lut->domain_min[1];
+        float blue  = ( pix[2] * ( lut->dimension - 1 ) / 65536.0 / ( lut->domain_max[2] - lut->domain_min[2] ) ) + lut->domain_min[2];
 
         //x0
         uint16_t r0 = (uint16_t)red;
