8ef570fdff7fad60cae819c41b1599b760cd4767
masc4ii <markus.schweitzer@gmx.de>
2019-03-18 21:16:40 +0100

    v1.6

--- platform/qt/MainWindow.cpp
+++ platform/qt/MainWindow.cpp
@@ -41,8 +41,8 @@
 #include "SingleFrameExportDialog.h"
 
 #define APPNAME "MLV App"
-#define VERSION "1.5"
-#define GITVERSION "QTv1.5"
+#define VERSION "1.6"
+#define GITVERSION "QTv1.6"
 
 #define FACTOR_DS       22.5
 #define FACTOR_LS       11.2
