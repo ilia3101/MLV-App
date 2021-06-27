7de73dec685855c511921c618941f917650802b2
masc4ii <markus.schweitzer@gmx.de>
2021-06-27 20:32:35 +0200

    v1.13

--- osx_installer/BuildInstaller.sh
+++ osx_installer/BuildInstaller.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 #Copy deployed "MLV App.app" to ./App directory before running this
 #Copy content from create-dmg (https://github.com/andreyvit/create-dmg) to ./create-dmg-master
-test -f "MLVApp.v1.12.OSX.dmg" && rm "MLVApp.v1.12.OSX.dmg"
+test -f "MLVApp.v1.13.OSX.dmg" && rm "MLVApp.v1.13.OSX.dmg"
 create-dmg-master/create-dmg \
---volname "MLVApp v1.12 Installer" \
+--volname "MLVApp v1.13 Installer" \
 --volicon "app/MLV App.app/Contents/Resources/MLVAPP.icns" \
 --background "dmg-background.png" \
 --window-pos 200 120 \
@@ -12,5 +12,5 @@ create-dmg-master/create-dmg \
 --icon "MLV App.app" 165 175 \
 --hide-extension "MLV App.app" \
 --app-drop-link 495 175 \
-"MLVApp.v1.12.OSX.dmg" \
+"MLVApp.v1.13.OSX.dmg" \
 "./app/"
