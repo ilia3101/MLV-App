e2ef906af3b23274ab4f6d08961c282c048f909a
masc4ii <markus.schweitzer@gmx.de>
2023-12-27 10:23:43 +0100

    Create BuildInstallerGithubAction.sh

---
+++ osx_installer/BuildInstallerGithubAction.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+#Copy deployed "MLV App.app" to ./App directory before running this
+#Copy content from create-dmg (https://github.com/andreyvit/create-dmg) to ./create-dmg-master
+test -f "MLVApp.OSX.dmg" && rm "MLVApp.OSX.dmg"
+create-dmg \
+--volname "MLVApp Installer" \
+--volicon "app/MLV App.app/Contents/Resources/MLVAPP.icns" \
+--background "dmg-background.png" \
+--window-pos 200 120 \
+--window-size 660 400 \
+--icon-size 100 \
+--icon "MLV App.app" 165 175 \
+--hide-extension "MLV App.app" \
+--app-drop-link 495 175 \
+"MLVApp.OSX.dmg" \
+"./app/"
