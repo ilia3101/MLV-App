75adf9e3b948a13afa0c168c28ec992a583ff2ae
Dobfek <dobfek@hotmail.com>
2025-01-25 00:06:53 +0100

    Inform user about export failure If vidstab is used and fails, it does not try to export the file.

--- platform/qt/MainWindow.cpp
+++ platform/qt/MainWindow.cpp
@@ -1711,6 +1711,7 @@ void MainWindow::writeSettings()
 //Start Export via Pipe
 void MainWindow::startExportPipe(QString fileName)
 {
+    bool staberr = false;
     //ffmpeg existing?
     {
 #if defined __linux__ && !defined APP_IMAGE
@@ -2162,7 +2163,8 @@ void MainWindow::startExportPipe(QString fileName)
             //Close pipe
             if( pclose( pPipeStab ) != 0 )
             {
-                QMessageBox::critical( this, tr( "File export failed" ), tr( "FFmpeg closed unexpectedly during stabilization.\n\nFile %1 was not exported." ).arg( fileName ) );
+                staberr = true;
+                QMessageBox::critical( this, tr( "File export failed" ), tr( "FFmpeg closed unexpectedly during stabilization.\n\nFile %1 was not exported completely." ).arg( fileName ) );
             }
             free( imgBufferScaled );
             free( imgBuffer );
@@ -2612,6 +2614,8 @@ void MainWindow::startExportPipe(QString fileName)
         program.insert( program.indexOf( "-c:v" ), pass3 );
     }
 
+	if( ( m_exportQueue.first()->vidStabEnabled() && staberr == false ) || !m_exportQueue.first()->vidStabEnabled() )
+    {
     //Try to open pipe
     FILE *pPipe;
     //qDebug() << "Call ffmpeg:" << program;
@@ -2704,11 +2708,12 @@ void MainWindow::startExportPipe(QString fileName)
         //Close pipe
         if( pclose( pPipe ) != 0 )
         {
-            QMessageBox::critical( this, tr( "File export failed" ), tr( "FFmpeg closed unexpectedly during export.\n\nFile %1 was not exported." ).arg( fileName ) );
+            QMessageBox::critical( this, tr( "File export failed" ), tr( "FFmpeg closed unexpectedly during export.\n\nFile %1 was not exported completely." ).arg( fileName ) );
         }
         free( imgBufferScaled );
         free( imgBuffer );
     }
+	}
 
     //Delete wav file
     QFile *file = new QFile( wavFileName );
