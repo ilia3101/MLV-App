0090681d12cf58a52da98141d6f4a5878bbd9b9a
masc4ii <markus.schweitzer@gmx.de>
2025-06-29 19:39:41 +0200

    3D LUT processing 20% speed up

--- src/processing/cube_lut.c
+++ src/processing/cube_lut.c
@@ -16,21 +16,23 @@
 //Switch here between trilinear and tetrahedral interpolation
 //#define USE_TRILIN_INT
 
+#define LUT3D_INDEX(r, g, b, dim, dim2) (((r) + (g)*(dim) + (b)*(dim2)) * 3)
+
 // //////////////
 //Interpolation functions
-float lerp(float x, float x1, float x2, float q00, float q01) {
+inline float lerp(float x, float x1, float x2, float q00, float q01) {
     if( ( x2 - x1 ) == 0 ) return q00;
     else return ( ( ( x2 - x ) / ( x2 - x1 ) ) * q00 ) + ( ( ( x - x1 ) / ( x2 - x1 ) ) * q01 );
 }
 
-float biLerp(float x, float y, float q11, float q12, float q21, float q22, float x1, float x2, float y1, float y2) {
+inline float biLerp(float x, float y, float q11, float q12, float q21, float q22, float x1, float x2, float y1, float y2) {
   float r1 = lerp(x, x1, x2, q11, q21);
   float r2 = lerp(x, x1, x2, q12, q22);
 
   return lerp(y, y1, y2, r1, r2);
 }
 
-float triLerp(float x, float y, float z, float q000, float q001, float q010, float q011, float q100, float q101, float q110, float q111, float x1, float x2, float y1, float y2, float z1, float z2) {
+inline float triLerp(float x, float y, float z, float q000, float q001, float q010, float q011, float q100, float q101, float q110, float q111, float x1, float x2, float y1, float y2, float z1, float z2) {
   float x00 = lerp(x, x1, x2, q000, q100);
   float x10 = lerp(x, x1, x2, q010, q110);
   float x01 = lerp(x, x1, x2, q001, q101);
@@ -199,27 +201,6 @@ void unload_lut( lut_t *lut )
     lut->dimension = 0;
 }
 
-//Read out a point from the 1dlut (input: 0..dimension-1)
-float getLut1dPoint( lut_t *lut, uint16_t inPos, uint8_t outChannel )
-{
-    if( inPos >= lut->dimension ) inPos = lut->dimension - 1;
-    return lut->cube[ inPos * 3 + outChannel ];
-}
-
-//Read out a point from the 3dlut (input: 0..dimension-1)
-float getLut3dPoint( lut_t *lut, uint16_t rIn, uint16_t gIn, uint16_t bIn, uint8_t outChannel )
-{
-    if( rIn >= lut->dimension ) rIn = lut->dimension - 1;
-    if( gIn >= lut->dimension ) gIn = lut->dimension - 1;
-    if( bIn >= lut->dimension ) bIn = lut->dimension - 1;
-    uint32_t red0_offs = rIn;
-    uint32_t green0_offs = gIn * lut->dimension;
-    uint32_t blue0_offs = bIn * lut->dimension * lut->dimension;
-    uint32_t offset0 = ( red0_offs + green0_offs + blue0_offs ) * 3;
-
-    return lut->cube[ offset0 + outChannel ];
-}
-
 //Apply LUT on picture
 void apply_lut(lut_t *lut, int width, int height, uint16_t *image)
 {
@@ -230,12 +211,19 @@ void apply_lut(lut_t *lut, int width, int height, uint16_t *image)
     float factor1 = (float)lut->intensity / 100.0f;
     float factor2 = 1.0 - factor1;
 
+    const uint32_t dim = lut->dimension;
+    const uint32_t dim2 = dim * dim;
+    float *cube = lut->cube;
+    float factorA = ( lut->dimension - 1 ) / 65536.0 / ( lut->domain_max[0] - lut->domain_min[0] );
+    float factorB = ( lut->dimension - 1 ) / 65536.0 / ( lut->domain_max[1] - lut->domain_min[1] );
+    float factorC = ( lut->dimension - 1 ) / 65536.0 / ( lut->domain_max[2] - lut->domain_min[2] );
+
     for (uint16_t * pix = image; pix < end; pix += 3)
     {
         //x
-        float red   = ( pix[0] * ( lut->dimension - 1 ) / 65536.0 / ( lut->domain_max[0] - lut->domain_min[0] ) ) - lut->domain_min[0];
-        float green = ( pix[1] * ( lut->dimension - 1 ) / 65536.0 / ( lut->domain_max[1] - lut->domain_min[1] ) ) - lut->domain_min[1];
-        float blue  = ( pix[2] * ( lut->dimension - 1 ) / 65536.0 / ( lut->domain_max[2] - lut->domain_min[2] ) ) - lut->domain_min[2];
+        float red   = ( pix[0] * factorA ) - lut->domain_min[0];
+        float green = ( pix[1] * factorB ) - lut->domain_min[1];
+        float blue  = ( pix[2] * factorC ) - lut->domain_min[2];
 
         //x0
         uint16_t r0 = (uint16_t)red;
@@ -246,16 +234,24 @@ void apply_lut(lut_t *lut, int width, int height, uint16_t *image)
         uint16_t g1 = g0 + 1;
         uint16_t b1 = b0 + 1;
 
+        //Limit
+        if( r0 >= lut->dimension ) r0 = lut->dimension - 1;
+        if( g0 >= lut->dimension ) g0 = lut->dimension - 1;
+        if( b0 >= lut->dimension ) b0 = lut->dimension - 1;
+        if( r1 >= lut->dimension ) r1 = lut->dimension - 1;
+        if( g1 >= lut->dimension ) g1 = lut->dimension - 1;
+        if( b1 >= lut->dimension ) b1 = lut->dimension - 1;
+
         if( lut->is3d == 0 )
         {
             //Linear Interpolation
             //y0 & //y1
-            float pix00 = getLut1dPoint( lut, r0, 0 );
-            float pix01 = getLut1dPoint( lut, r1, 0 );
-            float pix10 = getLut1dPoint( lut, g0, 1 );
-            float pix11 = getLut1dPoint( lut, g1, 1 );
-            float pix20 = getLut1dPoint( lut, b0, 2 );
-            float pix21 = getLut1dPoint( lut, b1, 2 );
+            float pix00 = cube[r0 * 3 + 0];
+            float pix01 = cube[r1 * 3 + 0];
+            float pix10 = cube[g0 * 3 + 1];
+            float pix11 = cube[g1 * 3 + 1];
+            float pix20 = cube[b0 * 3 + 2];
+            float pix21 = cube[b1 * 3 + 2];
             //3x Linear Interpolation
             float a = lerp( red,   r0, r1, pix00, pix01 );
             float b = lerp( green, g0, g1, pix10, pix11 );
@@ -271,14 +267,14 @@ void apply_lut(lut_t *lut, int width, int height, uint16_t *image)
             //Trilinear Interpolation
             for( int i = 0; i < 3; i++ )
             {
-                float q000 = getLut3dPoint( lut, r0, g0, b0, i );
-                float q001 = getLut3dPoint( lut, r0, g0, b1, i );
-                float q010 = getLut3dPoint( lut, r0, g1, b0, i );
-                float q011 = getLut3dPoint( lut, r0, g1, b1, i );
-                float q100 = getLut3dPoint( lut, r1, g0, b0, i );
-                float q101 = getLut3dPoint( lut, r1, g0, b1, i );
-                float q110 = getLut3dPoint( lut, r1, g1, b0, i );
-                float q111 = getLut3dPoint( lut, r1, g1, b1, i );
+                float q000 = cube[LUT3D_INDEX(r0, g0, b0, dim, dim2) + i];
+                float q001 = cube[LUT3D_INDEX(r0, g0, b1, dim, dim2) + i];
+                float q010 = cube[LUT3D_INDEX(r0, g1, b0, dim, dim2) + i];
+                float q011 = cube[LUT3D_INDEX(r0, g1, b1, dim, dim2) + i];
+                float q100 = cube[LUT3D_INDEX(r1, g0, b0, dim, dim2) + i];
+                float q101 = cube[LUT3D_INDEX(r1, g0, b1, dim, dim2) + i];
+                float q110 = cube[LUT3D_INDEX(r1, g1, b0, dim, dim2) + i];
+                float q111 = cube[LUT3D_INDEX(r1, g1, b1, dim, dim2) + i];
 
                 float out = triLerp(red, green, blue, q000, q001, q010, q011, q100, q101, q110, q111, r0, r1, g0, g1, b0, b1);
 
@@ -293,14 +289,14 @@ void apply_lut(lut_t *lut, int width, int height, uint16_t *image)
 
             for( uint8_t i = 0; i < 3; i++ )
             {
-                float q000 = getLut3dPoint( lut, r0, g0, b0, i );
-                float q001 = getLut3dPoint( lut, r0, g0, b1, i );
-                float q010 = getLut3dPoint( lut, r0, g1, b0, i );
-                float q011 = getLut3dPoint( lut, r0, g1, b1, i );
-                float q100 = getLut3dPoint( lut, r1, g0, b0, i );
-                float q101 = getLut3dPoint( lut, r1, g0, b1, i );
-                float q110 = getLut3dPoint( lut, r1, g1, b0, i );
-                float q111 = getLut3dPoint( lut, r1, g1, b1, i );
+                float q000 = cube[LUT3D_INDEX(r0, g0, b0, dim, dim2) + i];
+                float q001 = cube[LUT3D_INDEX(r0, g0, b1, dim, dim2) + i];
+                float q010 = cube[LUT3D_INDEX(r0, g1, b0, dim, dim2) + i];
+                float q011 = cube[LUT3D_INDEX(r0, g1, b1, dim, dim2) + i];
+                float q100 = cube[LUT3D_INDEX(r1, g0, b0, dim, dim2) + i];
+                float q101 = cube[LUT3D_INDEX(r1, g0, b1, dim, dim2) + i];
+                float q110 = cube[LUT3D_INDEX(r1, g1, b0, dim, dim2) + i];
+                float q111 = cube[LUT3D_INDEX(r1, g1, b1, dim, dim2) + i];
 
                 float out;
                 if( green >= blue && blue >= red ) //T1
